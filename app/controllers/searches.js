8087d923b7e32daa05940ebc1ebccbee290dd5dd
Girish Chandra <bobbyhimvihar@gmail.com>
2018-05-15 06:23:18 -0400

    review three

--- app/controllers/searches.js
+++ app/controllers/searches.js
@@ -2,16 +2,15 @@ import Controller from '@ember/controller';
 import EmberObject, { computed } from '@ember/object';
 import { oneWay } from '@ember/object/computed';
 import { empty } from '@ember/object/computed';
+import { alias } from '@ember/object/computed';
 
 
 export default Controller.extend({
  queryParams: ['searchkey'],
  searchkey:null,
  keywordSearches: null,
- resultLength: computed.alias('model.length'),
- searchKeyChange: computed('searchkey', function(){
-   return this.get('searchkey');
- }),
+ resultLength: alias('model.length'),
+ searchKeyChange: alias('searchkey'),
  keywordSearches: oneWay('searchKeyChange'),
  isDisabled: empty('keywordSearches'),
 
